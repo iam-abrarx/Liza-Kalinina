dcd9a1432595db6a506846113d8ddc041a05baac
Abrar Al Sayem <abraralsayem97@gmail.com>
2026-03-25 11:45:28 +0600

    Fix: next.config type error to ensure build success.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isExport = process.env.EXPORT === 'true';
 console.log('Build Mode:', isExport ? 'export' : 'default');
 
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   output: isExport ? 'export' : undefined,
   basePath: isExport ? '/Liza-Kalinina' : undefined,
   assetPrefix: isExport ? '/Liza-Kalinina/' : undefined,
