8c230e2ff0bbf0d97788120881a22069f9802ae0
Abrar Al Sayem <abraralsayem97@gmail.com>
2026-03-25 12:07:06 +0600

    Cleanup: remove export logic for better Vercel compatibility.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
-import type { NextConfig } from "next";
-
-const isExport = process.env.EXPORT === 'true';
-console.log('Build Mode:', isExport ? 'export' : 'default');
-
 const nextConfig: any = {
-  output: isExport ? 'export' : undefined,
-  basePath: isExport ? '/Liza-Kalinina' : undefined,
-  assetPrefix: isExport ? '/Liza-Kalinina/' : undefined,
   images: {
     unoptimized: true,
   },
